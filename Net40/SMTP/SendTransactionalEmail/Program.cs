c4ceab93864cd047c57b54b78f6dcc094fb6c897
Andrew Gretton <andrew.gretton@dotmailer.com>
2016-05-05 16:34:50 +0100

    Adding a transactional email sample

---
+++ Net40/SMTP/SendTransactionalEmail/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendTransactionalEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // credentials
+            var smtpHostname = "r1-smtp.dotmailer.com";
+            var transactionalApiUsername = "your-transactional-user@apiconnector.com";
+            var transactionalApiPassword = "your-password";
+
+            using (var client = new SmtpClient(smtpHostname))
+            {
+                client.Credentials = new NetworkCredential(transactionalApiUsername, transactionalApiPassword);
+                client.EnableSsl = true;
+                using (var msg = new MailMessage("from@yourcompany.com", "youraddress@youraddress.com"))
+                {
+                    msg.Subject = "Hello world";
+                    msg.Body = "<b>Hello</b> from dotmailer transactional email";
+                    msg.IsBodyHtml = true;
+                    client.Send(msg);
+                }
+            }
+
+        }
+    }
+}
